7cb8d5fc4fc0db1e5c941de4b58a5cfc26dcd347
shinjoongeun <solie0705@gmail.com>
2021-08-29 15:01:54 +0900

    수정 컴포넌트 추가

---
+++ src/inc/Modal.js
@@ -0,0 +1,11 @@
+import React,{ useState } from 'react';
+
+const Modal = ({ selectedData, handleCancel, handleEditSubmit }) => {
+    const [edited, setEdited] = useState(selectedData);
+
+    const onCancel = () => {
+        handleCancel();
+    }
+
+    const on
+}
