8c2fb55734a9a4a3ad451768b78b8161c662bf69
shinjoongeun <85258970+shinjoongeun@users.noreply.github.com>
2021-09-01 17:55:10 +0900

    Modify 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Main from "./inc/Main";
 import Save from "./inc/Save";
+import Modify from "./inc/Modify";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/main" exact={true} component={Main} />
         <Route path="/save" exact={true} component={Save} />
+        <Route path="/modify" exact={true} component={Modify} />
       </Switch>
     </div>
   );
